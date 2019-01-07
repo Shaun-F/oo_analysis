550a5bf34bf6f89a8aeed8d6acf3f00eeea23adc
Shaun <shaundbfell@gmail.com>
2019-01-07 11:40:40 -0800

    updated grand analysis scripts.

--- analysis/grand_analysis.py
+++ analysis/grand_analysis.py
@@ -7,7 +7,33 @@ import sys
 sys.path.append("..")
 import analysis.scan_analysis
 import analysis.coaddition
-
+import h5py
 # seed result grand spectra
-# cycle over scans, analyzing each
+
 # add scan results to grand spectra
+
+file = h5py.File(b"../data/raw/run1a_data.hdf5", "r+")
+
+#pull grand spectra or create it. Then pull data group
+if "grand_spectra_run1a" in file.keys():
+	grand_spectra = file["grand_spectra_run1a"]
+else:
+	grand_spectra = file.create_dataset("grand_spectra_run1a", data = [], dtype=float, chunks = True, maxshape=None)
+
+data_group = file["digitizer_log_run1a"]
+
+# cycle over scans, analyzing each
+for dataset in data_group.keys():
+	data = dataset[...]
+
+
+
+
+
+
+
+
+
+
+
+
