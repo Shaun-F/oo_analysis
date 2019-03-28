46933ad65431e74c160ad42a2181fc81155da411
Shaun <shaundbfell@gmail.com>
2019-03-28 13:05:31 -0700

    optimized various subprocesses

--- control/core.py
+++ control/core.py
@@ -4,13 +4,13 @@ core.py: holds  control routines for analysis
 Created by: Erik Lentz
 Creation Date: 6/1/18
 """
-import sys
+import sys; import os
 sys.path.append("../experiment")
 sys.path.append("..")
 from control.param_parser import parser
 from experiment.get_squid_dataset import get_squid_dataset
 from experiment.calc_sys_temp_offline import calc_sys_temp
-import time
+import time; import datetime
 import argparse
 
 ############# Argument parsing
@@ -55,13 +55,12 @@ class core_analysis():
 		self.keys = [i for i in self.dig_dataset.keys() if i not in self.no_axion_log] #Was originally dig_dataset,but some digitizer logs didnt have associated axion logs.
 		pulldata_stop = time.time()
 		
-		
 		self.output_file = "../output/grand_spectra.dat"
 		print("Loading data successful. It took {0:0.3f} seconds. Beginning analysis of {1} scans".format((pulldata_stop-pulldata_start), len(self.keys)))
 		if self.paritioned:
 			print("Note: Dataset was paritioned")
 		if reset and 'grand_spectra_run1a' in self.h5py_file.keys():
-			del self.h5py_file['grand_spectra_run1a']
+			del self.h5py_file['grand_spectra_run1a']	
 		
         # derive necessary experiment data structures (put into dig_dataset)
 		#Populate parameter dict's with dataset attributes.
@@ -94,8 +93,6 @@ class core_analysis():
 	def execute(self, timeit=False):
 		try:
 			# sets all calculations in motion
-			if len(self.keys)==0:
-				return print("No scans to analyze.")
 			self.meta_analysis = [timeit]
 			self.collect_bad_scans()
 			
@@ -110,11 +107,13 @@ class core_analysis():
 			signals_start = time.time()
 			self.signal_dataset = signals.generate(self)
 			signals_stop=time.time()
+			
 			print("signal generating complete. Beginning Analysis")
 			import analysis
 			analysis_start=time.time()
 			self.analysis_dataset, ncut = analysis.grand_spectra(self)
 			analysis_stop=time.time()
+			
 			#import MCMC
 			# perform MCMC analysis
 			#import analytics
@@ -125,7 +124,9 @@ class core_analysis():
 				self.meta_analysis.append("Total analysis time of {0} scans is {1:03f} seconds".format(len(self.keys), analysis_stop - analysis_start))
 				[print(x) for x in self.meta_analysis[1:]]
 				print("#################################### End of Meta-analysis###########################\n\n")
-				with open("../meta/analysis_statistics.txt", "w") as f:
+				date = datetime.datetime.now()
+				filename = "../meta/analysis_statistics" + "(" + str(len(self.keys)) + "_scans)(" + str(date.year) + "-" + str(date.month) + "-" + str(date.day) + "_" + str(date.hour) + "-" + str(date.minute) + "-" + str(date.second) + ").txt"
+				with open(filename, "w") as f:
 					for x in self.meta_analysis[1:]:
 						f.write(str(x) + "\n")
 			self.output()
@@ -135,10 +136,17 @@ class core_analysis():
 			
 			print(string)
 			return None
-		except (KeyError, TypeError) as error:
+		except (KeyError, TypeError, SyntaxError) as error:
 			self.h5py_file.close() #prevent corruption on break
-			print(error)
+			print("Execution failed with error: \n {0}".format(error))
 			raise
+		except KeyboardInterrupt:
+			self.h5py_file.close()
+			print("Interrupted")
+			try:
+				sys.exit(0)
+			except SystemExit:
+				os._exit(0)
 			
 
 
